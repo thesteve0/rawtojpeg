97aabe6ca00d80cb145c7bc49c3dea0664764529
Steven Pousty <steve.pousty@gmail.com>
2023-06-20 08:39:43 -0700

    Adding comments

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ print("hello")
 rawimg = rawpy.imread("C:/Users/steve/tmp\photo-rename-tester/orig-images/2017/DSC_0001.NEF")
 print(rawimg.sizes)
 rawimg.close()
+
+
+# basic works - need to export to JPG, rename the file, and then walk the tree.
+# Also need to detect if there are two files with the same name but one is .jpg and the other is raw. If so then just move the jpg but tag
+# both with the same tags
+#
+# Maybe we don't rename the file, maybe we actually just embed the path in the JPG exif data (or header or something)
